270d7b049584de2afd7a46d2cb70add00e105def
fpahsdnjxj <enjin0222@naver.com>
2025-05-11 22:10:22 +0900

    backend restructuring

--- backend/src/main/java/com/solsoll/ttarang/backend/config/FirebaseConfig.java
+++ backend/src/main/java/com/solsoll/ttarang/backend/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import jakarta.annotation.PostConstruct;
 import java.io.FileInputStream;
@@ -15,10 +16,10 @@ public class FirebaseConfig {
     @PostConstruct
     public void initialize() {
         try {
-            // 이미 초기화된 앱이 있으면 다시 초기화하지 않음
             if (FirebaseApp.getApps().isEmpty()) {
-                FileInputStream serviceAccount =
-                        new FileInputStream("src/main/resources/firebase-service-account.json");
+                ClassPathResource resource = new ClassPathResource("firebase-service-account.json");
+                FileInputStream serviceAccount = new FileInputStream(resource.getFile());
+
 
                 FirebaseOptions options = FirebaseOptions.builder()
                         .setCredentials(GoogleCredentials.fromStream(serviceAccount))
